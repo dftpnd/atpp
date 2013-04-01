b7ef5527471214a2679361c11072451770beee75
Maks Gryaznov <lkdnvc@gmail.com>
2013-04-01 09:31:19 +0400

    pre pull

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 $ds = DIRECTORY_SEPARATOR;
-
 ini_set('date.timezone', 'Europe/Moscow');
 $yii = dirname(__FILE__) . $ds . 'yii-1.1.12' . $ds . 'framework' . $ds . 'yii.php';
 $config = dirname(__FILE__) . $ds . 'protected' . $ds . 'config' . $ds . 'main.php';
@@ -12,6 +11,7 @@ ini_set('error_reporting', 'E_ALL');
 ini_set('date.timezone', 'Europe/Moscow');
 ini_set('error_reporting', '-1');
 require_once($yii);
+
 Yii::createWebApplication($config)->run();
 
 
