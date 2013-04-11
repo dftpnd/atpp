faab410b67fd1d7a143fd3fb7a4f4d83184bf4bd
Gryaznov Maksim <lkdnvc@gmail.com>
2013-04-11 21:32:52 +0400

    prere

--- protected/views/library/index.php
+++ protected/views/library/index.php
@@ -66,7 +66,7 @@
                     $caf_name = $predmet->cafedra->name;
 
                     foreach ($institute as $key => $v) {
-                       $ins_name = $v[0][0]; 
+                       $ins_name = var_dump($v[0]); 
                     }
                 } else {
                     $caf_name = '';
