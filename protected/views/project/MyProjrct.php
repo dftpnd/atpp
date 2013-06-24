cfe9d7ad4a0cf67b7a6a32dad05a66105bdda5a2
Gryaznov Maksim <lkdnvc@gmail.com>
2013-06-24 21:45:06 +0400

    asdasd

--- protected/views/project/MyProjrct.php
+++ protected/views/project/MyProjrct.php
@@ -2,7 +2,7 @@
 <html class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" lang="en"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>CSS Piffle - Designing beautiful interfaces without writing any code</title>
+    <title>Интерактивные электронные комплексы на основе дополненной реальности </title>
 
     <link rel="stylesheet" href="http://csspiffle.com/static/css/style.css">
     <link rel="shortcut icon" href="http://csspiffle.com/static/favicon.png">
