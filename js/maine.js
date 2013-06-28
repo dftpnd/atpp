f8bd1c56ac5c993ded09e2732dc219c4ef7146c3
Maks Gryaznov <m.gryaznov@example.com>
2013-06-28 12:12:49 +0400

    google script

--- js/maine.js
+++ js/maine.js
@@ -1,3 +1,16 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38492172-1']);
+_gaq.push(['_trackPageview']);
+    
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
 /*====*/
 var notice_green = '1';
 var notice_yellow = '2';
