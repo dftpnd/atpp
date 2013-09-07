c13856915a5b4f5b681ae2ea581398d275091544
Maks Gryaznov <lkdnvc@gmail.com>
2013-09-07 09:46:21 +0400

    bug fix

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -10,24 +10,17 @@ class Controller extends SBaseController
     public function init()
     {
         $cs = Yii::app()->clientScript;
-//        Yii::app()->clientScript->scriptMap['jquery.js'] = false;
-//        Yii::app()->clientScript->scriptMap['jquery-ui.js'] = false;
-        Yii::app()->clientScript->scriptMap['jquery.yiiactiveform.js'] = false;
-        Yii::app()->clientScript->scriptMap['jquery.ba-bbq.js'] = false;
-
-
-//        $cs->registerScriptFile($this->createUrl('/js/jquery-2.0.3.min.js'));
-//        $cs->registerScriptFile($this->createUrl('/js/jquery-ui.min.js'));
-        $cs->registerScriptFile($this->createUrl('/js/yiiactiveform.js'));
+        $cs->registerCoreScript('jquery');
+        $cs->registerCoreScript('jquery.ui');
         $cs->registerScriptFile($this->createUrl('/js/jquery.md5.js'));
         $cs->registerScriptFile($this->createUrl('/js/jquery.scrollTo.min.js'));
         $cs->registerScriptFile($this->createUrl('/js/nprogress.js'));
         $cs->registerScriptFile($this->createUrl('/js/fileuploader.js'));
         $cs->registerScriptFile($this->createUrl('/js/amcharts.js'));
         $cs->registerScriptFile($this->createUrl('/js/amcharts_ui.js'));
-        $cs->registerScriptFile($this->createUrl('/js/jq-scrool_old.js'));
+        $cs->registerScriptFile($this->createUrl('/js/fixed_table_header.js'));
         $cs->registerScriptFile($this->createUrl('/js/door.js'));
-        $cs->registerScriptFile($this->createUrl('/js/maine.js'));
+        $cs->registerScriptFile($this->createUrl('/js/main.js'));
     }
 
 }
