65dfba41ce5ac781f010152b598bf0564d6bf2b3
Maks Gryaznov <lkdnvc@gmail.com>
2014-05-16 21:37:42 +0400

    forun fix

--- protected/views/forum/view.php
+++ protected/views/forum/view.php
@@ -9,8 +9,10 @@ $this->widget('zii.widgets.CListView', array(
     )
 ));
 ?>
+<?php if (count($comments) > 0): ?>
     <h1>Комментарии:</h1>
     <a name="comments"></a>
+<?php endif; ?>
 <?php
 //all comment
 
@@ -26,8 +28,9 @@ foreach ($comments as $comment) {
 ?>
 
 <?php if (Yii::app()->user->isGuest): ?>
-    <div style="margin-left: 150px">Только зарегистрированные пользователи могут оставлять комментарии. <span class="classic"
-                                                                                            onclick='EnterSite()'>Войдите</span>,
+    <div style="margin-left: 150px">Только зарегистрированные пользователи могут оставлять комментарии. <span
+            class="classic"
+            onclick='EnterSite()'>Войдите</span>,
         пожалуйста.
     </div>
 <?php else: ?>
