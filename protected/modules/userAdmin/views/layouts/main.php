9d18962e7e913907e7eb5d5ce4933d1488d5d9bc
Maks Gryaznov <lkdnvc@gmail.com>
2014-05-24 12:58:16 +0400

    rename css

--- protected/modules/userAdmin/views/layouts/main.php
+++ protected/modules/userAdmin/views/layouts/main.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="language" content="en"/>
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/spizdel.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/adminhub.css"/>
 
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/admin.css"/>
 
